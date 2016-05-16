698412eb65e5f15c10271a3f2a135f6d64221c61
heyitskevinliu <liukevin85@gmail.com>
2016-05-16 11:23:27 -0700

    Update run.py

--- run.py
+++ run.py
@@ -34,4 +34,4 @@ def show_messages():
     return str(messages)
     
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=80)
